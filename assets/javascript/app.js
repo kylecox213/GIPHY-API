4767b029c868e0f12d5e14158b98dfff21af5471
Kyle Cox <kylecox213@gmail.com>
2019-06-25 15:12:26 -0400

    app.js still needs some minor adjustments to get app functioning properly; framework for  app.js is complete; still need to add toggle function for start/stop animation for user on.click events; style css and reset css have not yet been added; app nearly complete and functional

---
+++ assets/javascript/app.js
@@ -0,0 +1,109 @@
+
+
+// Event listener for all button elements
+// $("button").on("click", function() {
+
+
+var nesGames = ["The Legend of Zelda", "Metroid", "Jackal", "Super Mario Bros. 3", "Ghosts N' Goblins", "Heavy Barrel",
+    "Metal Gear", "Guerilla War", "Ikari Warriors", "Star Soldier"];
+
+function displayGameInfo() {
+
+
+    // may not need the .attr("data-name") for var nesGame
+    var nesGame = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=nintendo+games&api_key=cZWfcmU9Q4EPAouDfubWCyrZEdNPwAKf&limit=10"
+
+
+    // AJAX call to the giphy API to retrieve "Get" the requested game data and "then", carry out f(response)"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+
+        // After the data comes back from the API
+    }).then(function (response) {
+
+        // Storing an array of results in the var results
+        var results = response.data;
+
+        // Looping over every result item
+        for (var i = 0; i < results.length; i++) {
+
+            // If statement for taking action ONLY (IF) the photo has an appropriate rating
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+
+
+                // Creating a div to hold the game
+                var gameDiv = $("<div class = game>");
+
+                // Storing the retrieved game rating data
+                var rating = results[i].rating;
+
+                // Creating an element to have the game rating displayed on the page
+                var pOne = $("<p>").text("Rating: " + rating);
+
+                // Displaying the game rating with the gameDiv
+                gameDiv.append(pOne);
+
+                // Creating an image tag
+                var gameImage = $("<img>");
+
+                // Giving the image tag an src attribute of a proprty pulled off of the result item
+                gameImage.attr("src", results[i].images.fixed_height_still.url);
+
+                // Appending the paragraph and gameImage to the "gameDiv" div 
+                gameDiv.append(pOne);
+                gameDiv.append(gameImage);
+
+                // Appending the gif to display following the gifs that preceded 
+                $("#games-view").append(gameDiv);
+
+                }
+            }
+        });
+    
+
+
+    // Function for displaying game data
+    function renderButtons() {
+
+        // Deleting the games prior to adding new games; prevention for repeat buttons)
+        $("#buttons-view").empty();
+
+        // Looping through the array of games
+        for (var i = 0; i < nesGames.length; i++) {
+
+            // Dynamically generating buttons for each game in the array
+            var a = $("<button>");
+            // Adding a class of game-btn to our button
+            a.addClass("game-btn");
+            // Adding a data-attribute
+            a.attr("data-name", nesGames[i]);
+            // Providing the initial button text
+            a.text(nesGames[i]);
+            // Adding the button to the buttons-view div
+            $("#buttons-view").append(a);
+        }
+    }
+
+    // This function handles events where a game button is clicked
+    $("#add-game").on("click", function (event) {
+        event.preventDefault();
+
+        // This line grabs the input from the textbox
+        var game = $("#game-input").val().trim();
+
+        // Adding game from the textbox to our array
+        nesGames.push(nesGame);
+
+        // Calling renderButtons which handles the processing of our game array
+        renderButtons();
+    });
+
+    // Adding a click event listener to all elements with a class of "game-btn"
+    $(document).on("click", ".game-btn", displayGameInfo);
+
+    // Calling the renderButtons function to display the intial buttons
+    renderButtons();
+    ;
+};
