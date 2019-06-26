21a09d2f99696354a5f309e6e0a52725be3dfbea
Kyle Cox <kylecox213@gmail.com>
2019-06-26 02:19:21 -0400

    fixed errors in code that were mistakenly made in the app.js file, buttons render when app is ran; giphs functionality restored; still need to correct empty button issue; as well as the toggle between data-still and data-animate; css stylings were added; reset.css was added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -46,76 +46,78 @@ $("#add-game").on("click", function (event) {
 
 // Adding a click event listener to all elements with a class of "game-btn"
 $(document).on("click", ".game-btn", function () {
-    {
 
 
-        // may not need the .attr("data-name") for var nesGame
-        var nesGame = $(this).attr("data-name").split(" ").join("+");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cZWfcmU9Q4EPAouDfubWCyrZEdNPwAKf&limit=10&q="
+
+    // may not need the .attr("data-name") for var nesGame
+    var nesGame = $(this).attr("data-name").split(" ").join("+");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cZWfcmU9Q4EPAouDfubWCyrZEdNPwAKf&limit=10&q="
 
 
-            // AJAX call to the giphy API to retrieve "Get" the requested game data and "then", carry out f(response)"
-            $.ajax({
-                url: queryURL + nesGame,
-                method: "GET"
+    // AJAX call to the giphy API to retrieve "Get" the requested game data and "then", carry out f(response)"
+    $.ajax({
+        url: queryURL + nesGame,
+        method: "GET"
 
-                // After the data comes back from the API
-            }).then(function (response) {
+        // After the data comes back from the API
+    }).then(function (response) {
 
-                // Storing an array of results in the var results
-                var results = response.data;
+        // Storing an array of results in the var results
+        var results = response.data;
 
-                // Looping over every result item
-                for (var i = 0; i < results.length; i++) {
+        // Looping over every result item
+        for (var i = 0; i < results.length; i++) {
 
-                    // If statement for taking action ONLY (IF) the photo has an appropriate rating
-                    // if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+            // If statement for taking action ONLY (IF) the photo has an appropriate rating
+            // if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
 
 
-                        // Creating a div to hold the game
-                        var gameDiv = $("<div class = game>");
+            // Creating a div to hold the game
+            var gameDiv = $("<div class = game>");
 
-                        // Storing the retrieved game rating data
-                        var rating = results[i].rating;
+            // Storing the retrieved game rating data
+            var rating = results[i].rating;
 
-                        // Creating an element to have the game rating displayed on the page
-                        var pOne = $("<p>").text("Rating: " + rating);
+            // Creating an element to have the game rating displayed on the page
+            var pOne = $("<p>").text("Rating: " + rating);
 
-                        // Displaying the game rating with the gameDiv
-                        gameDiv.append(pOne);
+            // Displaying the game rating with the gameDiv
+            gameDiv.append(pOne);
 
-                        // Creating an image tag
-                        var gameImage = $("<img>");
+            // Creating an image tag
+            var gameImage = $("<img>");
 
-                        // Giving the image tag an src attribute of a proprty pulled off of the result item
-                        gameImage.attr("src", results[i].images.fixed_height_still.url);
+            // Giving the image tag an src attribute of a proprty pulled off of the result item
+            gameImage.attr("src", results[i].images.fixed_height_still.url);
 
-                        // Appending the paragraph and gameImage to the "gameDiv" div 
-                        gameDiv.append(pOne);
-                        gameDiv.append(gameImage);
+            // Appending the paragraph and gameImage to the "gameDiv" div 
+            gameDiv.append(pOne);
+            gameDiv.append(gameImage);
 
-                        // Appending the gif to display following the gifs that preceded 
-                        $("#games-view").prepend(gameDiv);
+            // Appending the gif to display following the gifs that preceded 
+            $("#games-view").prepend(gameDiv);
+        }
 
-                    
-                }
-            });
-    }
+        $("<img>").on("click", function () {
+            var state = $(this).attr("data-state");
+            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            // Then, set the image's data-state to animate
+            // Else set src to the data-still value
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            };
+        });
 
+    });
 });
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
-;
-$(".game-btn").on("click", function() {
-var state = $(this).attr("data-state");
-// If the clicked image's state is still, update its src attribute to what its data-animate value is.
-// Then, set the image's data-state to animate
-// Else set src to the data-still value
-if (state === "still") {
-  $(this).attr("src", $(this).attr("data-animate"));
-  $(this).attr("data-state", "animate");
-} else {
-  $(this).attr("src", $(this).attr("data-still"));
-  $(this).attr("data-state", "still");
-};
+
+
+
+
